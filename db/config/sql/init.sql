e25471281cf83bfcdc38a0f1ca0861a8379680ce
lauvsong <hyehye9355@naver.com>
2022-02-03 18:58:08 +0900

    sql: sql 파일 단일화 (init.sql)

--- db/config/sql/init.sql
+++ db/config/sql/init.sql
@@ -39,3 +39,10 @@ create table favorites
         foreign key (user_id) references users (id)
 );
 
+insert into greenDB.tiers (id, name, value)
+values  (1, 'seed', 0),
+        (2, 'sprout', 50),
+        (3, 'leaf', 100),
+        (4, 'grass', 150),
+        (5, 'tree', 200),
+        (6, 'forest', 250);
