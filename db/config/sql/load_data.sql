20edd90b9fc2f57239370770a7199355b54f8f84
lauvsong <hyehye9355@naver.com>
2022-02-03 18:11:08 +0900

    chore: 디렉토리 구조 변경

---
+++ db/config/sql/load_data.sql
@@ -0,0 +1,7 @@
+insert into greenDB.tiers (id, name, value)
+values  (1, 'seed', 0),
+        (2, 'sprout', 50),
+        (3, 'leaf', 100),
+        (4, 'grass', 150),
+        (5, 'tree', 200),
+        (6, 'forest', 250);
