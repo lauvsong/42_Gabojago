646661e32bfd706959b7151188c13cff5e63ea0c
lauvsong <hyehye9355@naver.com>
2022-02-04 20:32:36 +0900

    fix conflict

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,35 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+var Product = require("../../models").product;
+var Categories = require("../../models").categories;
+
+// 모든 제품 리스트 조회
+router.get("/", async (req, res, next) => {
+  try {
+    const products = await Product.findAll(
+      {include: [{
+        model: 
+      }]}
+    );
+    console.log(products);
+    res.send(products);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// 모든 제품 리스트 조회 (조회수 내림차순 랭킹)
+router.get("/rank", async (req, res, next) => {
+  try {
+    const products = await Product.findAll();
+    console.log(products);
+    res.send(products);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
