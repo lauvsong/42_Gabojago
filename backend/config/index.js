b9d8a4957ab28a0abbf07afd0c61d01d7f1df24c
lauvsong <hyehye9355@naver.com>
2022-02-05 02:59:06 +0900

    chore: 데이터베이스 설정 변경

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,46 +1,46 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const env = process.env
+const env = process.env;
 
 const development = {
-    "username": "admin",
-    "password": "P@ssw0rd",
-    "database": "nodejs",
-    "host": "green-db",
-    "dialect": "mariadb",
-    "port": env.DB_PORT,
-    "define": {
-        "underscored": false,
-        "freezeTableName": false,
-        "charset": "utf8",
-        "dialectOptions": {
-            "collate": "utf8_general_ci"
-        },
-        "timestamps": true,
-        "paranoid": true
-    }
-}
+  username: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME,
+  host: env.DB_HOST,
+  dialect: "mariadb",
+  port: env.DB_PORT,
+  define: {
+    underscored: false,
+    freezeTableName: false,
+    charset: "utf8",
+    dialectOptions: {
+      collate: "utf8_general_ci",
+    },
+    timestamps: true,
+    paranoid: true,
+  },
+};
 
 const production = {
-    "username": "admin",
-    "password": "P@ssw0rd",
-    "database": "nodejs",
-    "host": "green-db",
-    "dialect": "mariadb",
-    "port": env.DB_PORT,
-    "define": {
-        "underscored": false,
-        "freezeTableName": false,
-        "charset": "utf8",
-        "dialectOptions": {
-            "collate": "utf8_general_ci"
-        },
-        "timestamps": true,
-        "paranoid": true
-    }
-}
+  username: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME,
+  host: env.DB_HOST,
+  dialect: "mariadb",
+  port: env.DB_PORT,
+  define: {
+    underscored: false,
+    freezeTableName: false,
+    charset: "utf8",
+    dialectOptions: {
+      collate: "utf8_general_ci",
+    },
+    timestamps: true,
+    paranoid: true,
+  },
+};
 
 module.exports = {
   development,
-  production
-}
+  production,
+};
