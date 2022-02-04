476251a70280452e00121eff3f47ddebcc10c043
lauvsong <hyehye9355@naver.com>
2022-02-04 23:01:17 +0900

    feat: product / products API 구현

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,34 +1,129 @@
 var express = require("express");
+const { sequelize } = require("../models");
 var router = express.Router();
 
-var Product = require("../../models").product;
-var Categories = require("../../models").categories;
+var Product = require("../models").product;
+var Categories = require("../models").categories;
+const { QueryTypes } = require("sequelize");
 
-// 모든 제품 리스트 조회
-router.get("/", async (req, res, next) => {
+// 바코드를 통한 특정 제품 조회
+router.get("/:bacode", async (req, res) => {
+  let query = `
+  SELECT p.*,c.name as category_name FROM products AS p
+  LEFT JOIN categories AS c
+  on p.category_id = c.id
+  WHERE p.bacode = :bacode
+  `;
+
   try {
-    const products = await Product.findAll(
-      {include: [{
-        model: 
-      }]}
-    );
-    console.log(products);
+    const products = await sequelize.query(query, {
+      replacements: { bacode: req.params.bacode },
+      type: QueryTypes.SELECT,
+      raw: true,
+    });
     res.send(products);
   } catch (error) {
     console.error(error);
-    next(error);
   }
 });
 
-// 모든 제품 리스트 조회 (조회수 내림차순 랭킹)
-router.get("/rank", async (req, res, next) => {
+// 특정 제품 조회
+router.get("/:id", async (req, res) => {
+  let query = `
+  SELECT p.*,c.name as category_name FROM products AS p
+  LEFT JOIN categories AS c
+  on p.category_id = c.id
+  WHERE p.id = :id
+  `;
+
   try {
-    const products = await Product.findAll();
-    console.log(products);
+    const products = await sequelize.query(query, {
+      replacements: { id: req.params.id },
+      type: QueryTypes.SELECT,
+      raw: true,
+    });
     res.send(products);
   } catch (error) {
     console.error(error);
-    next(error);
+  }
+});
+
+// 특정 제품 정보 전체 수정
+router.put("/:id", async (req, res) => {
+  let query = `
+  UPDATE products SET info = :info WHERE id = :id
+  `;
+
+  try {
+    await sequelize.query(query, {
+      replacements: { id: req.params.id, info: req.body },
+      type: QueryTypes.SELECT,
+      raw: true,
+    });
+    res.send();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// 특정 제품 삭제
+router.delete("/:id", async (req, res) => {
+  let query = `
+  DELETE products WHERE id = :id
+  `;
+
+  try {
+    await sequelize.query(query, {
+      replacements: { id: req.params.id },
+      type: QueryTypes.SELECT,
+      raw: true,
+    });
+    res.send();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// 특정 제품 조회수 +1 갱신
+router.patch("/:id", async (req, res) => {
+  let query = `
+  UPDATE products SET hit = :hit WHERE id = :id
+  `;
+
+  try {
+    await sequelize.query(query, {
+      replacements: { id: req.params.id, hit: req.params.hit + 1 },
+      type: QueryTypes.SELECT,
+      raw: true,
+    });
+    res.send();
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// 제품 등록
+router.post("/register", async (req, res) => {
+  let query = `
+  INSERT INTO products (category_id, name, hit, info, bacode)
+  values (:category_id, :name, :hit, :info, :bacode)
+  `;
+
+  try {
+    await sequelize.query(query, {
+      replacements: {
+        category_id: req.body.category_id,
+        name: req.body.name,
+        hit: req.body.hit,
+        info: req.body.info,
+        bacode: req.body.bacode,
+      },
+      type: QueryTypes.SELECT,
+      raw: true,
+    });
+    res.send();
+  } catch (error) {
+    console.error(error);
   }
 });
 
