e6ef70073325b9113eb5e814b035d7845f6f64b9
조소연 <84591000+algosipdahack@users.noreply.github.com>
2022-02-05 05:47:54 +0900

    Add response.js

--- backend/config/index.js
+++ backend/config/index.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 const env = process.env;
 
 const development = {
-  username: env.DB_USER,
-  password: env.DB_PASSWORD,
-  database: env.DB_NAME,
+  username: "admin",
+  password: "P@ssw0rd",
+  database: "nodejs",
   host: env.DB_HOST,
   dialect: "mariadb",
   port: env.DB_PORT,
